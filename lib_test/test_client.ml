997a10e37da46aad700d1203dbb4666bc9acfce3
Docker <firgeis@gmail.com>
2021-05-05 21:59:21 +0000

    New certificates

--- lib_test/test_client.ml
+++ lib_test/test_client.ml
@@ -223,7 +223,7 @@ let test_https_server_certs _ () =
     response;
   let* () = Helper_server.teardown server in
   (* Verify server cert rsa with SAN from cert string *)  
-  let* server, _ = Helper_server.listen ~http_port:8080 ~certfile:"server_san_rsa.pem" ~certkey:"server_san_rsa.key" () in
+  let* server, _ = Helper_server.listen ~http_port:8080 ~certfile:"server_rsa_san.pem" ~certkey:"server_rsa_san.key" () in
   let inchannel = open_in (Helper_server.cert_path // "ca.pem") in
   let certstring =
     really_input_string inchannel (in_channel_length inchannel)
